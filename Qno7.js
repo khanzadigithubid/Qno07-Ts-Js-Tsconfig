11149d32db61aed89f38376f0dda56f7663b0697
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-02-24 13:28:23 +0500

    Qno07 Ts/Js/Tsconfig and node js of 45 Assignments
    Qno07 Ts/Js/Tsconfig and node js of 45 Assignments

---
+++ Qno7.js
@@ -0,0 +1,16 @@
+"use strict";
+/*Number Eight: Write addition, subtraction, multiplication and division operations that
+each result in the number 8. Be sure to enclose your operations in print statements to see
+the results.*/
+// Addition
+let additionResult = 5 + 3;
+console.log(`Addition Result: ${additionResult}`);
+// Subtraction
+const subtractionResult = 10 - 2;
+console.log(`Subtraction Result: ${subtractionResult}`);
+// Multiplication
+const multiplicationResult = 4 * 2;
+console.log(`Multiplication Result: ${multiplicationResult}`);
+// Division
+const divisionResult = 16 / 2;
+console.log(`Division Result: ${divisionResult}`);
